f430e8bdfcf6b2f3097c12ee88430a66def94910
AgDevTroop <agra.widhianata@gmail.com>
2025-05-03 11:08:29 +0700

    Update login-script.js
    update untuk menggunakan jdoc

--- assets/scripts/login-script.js
+++ assets/scripts/login-script.js
@@ -1,18 +1,54 @@
-/* comment : membuat variabel untuk setiap elemen view. */
+/** @module Login-Script */
+/**
+* Membuat variabel loginFormElement untuk tampilan form.
+* @constant {HTMLElement}
+*/
 const loginFormElement = document.querySelector('#loginForm');
+
+
+/** 
+* Membuat variabel inputEmailElement untuk tampilan input email.
+* @constant {HTMLElement}
+*/
 const inputEmailElement = document.querySelector('#inputEmail');
+
+
+/** 
+* Membuat variabel inputPasswordElement untuk tampilan input password.
+* @constant {HTMLElement}
+*/
 const inputPasswordElement = document.querySelector('#inputPassword');
 
-/* comment : membuat variabel untuk menyimpan email dan password. */
+
+/** 
+ * Membuat variabel expectedEmail untuk menyimpan informasi email sementara.
+ * @constant {string}
+ */
 const expectedEmail = 'admin@dicoding.com';
+
+
+/** 
+ * Membuat variabel expectedPassword untuk menyimpan informasi password sementara.
+ * @constant {string}
+ */
 const expectedPassword = 'superpassword';
+
 
 /* comment : menambahkan aksi klik pada button. */
 loginFormElement.addEventListener('submit', function(event) {
   event.preventDefault();
 
-  /* comment : mendapatkan nilai dari masing-masing input (email dan password) ketika tombol ditekan. */
+  /** 
+   * Membuat variabel email untuk menyimpan nilai email yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const email = inputEmailElement.value;
+
+
+  /** 
+   * Membuat variabel password untuk menyimpan niali password yang didapatkan saat button ditekan.
+   * @constant {string}
+   */
   const password = inputPasswordElement.value;
 
   /* comment : memastikan bahwa nilai email dan password sesuai dengan nilai yang tersimpan. */
@@ -25,5 +61,6 @@ loginFormElement.addEventListener('submit', function(event) {
     
     /* comment : namun jika tidak sesuai maka akan menampilkan informasi bahwa input salah. */
     showPopUp();
+    
   }
 });
